b28f1edfa3a35d7f5e74df9adc21f40d46258615
Sanjay Suresh/JAM/Brenntag <sanjaysuresh15@gmail.com>
2019-10-08 19:12:45 -0500

    add

--- Belly_Button_Biodiversity/app.py
+++ Belly_Button_Biodiversity/app.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
 from flask import Flask, jsonify, render_template
-from Flask_SQLAlchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
